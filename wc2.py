fa31b6f62c93883e57c0c6617771902776bfae2e
naihua806 <naihua806@live.cn>
2013-06-05 17:45:18 +0900

    Update wc2.py

--- wc2.py
+++ wc2.py
@@ -1,18 +1,43 @@
 import sys
-f = sys.stdins = f.read()
+
+argc = len(sys.argv) 
+if argc == 1:     
+  f = sys.stdin
+elif argc == 2:    
+    try:
+      f = open(sys.argv[1], "rU")    
+    except IOError:        
+      print >>sys.stderr, "nl: %s: No such file or directory" % (sys.argv[1])        
+      sys.exit()
+else:    
+    print >>sys.stderr, "usage: wc [file]"    
+    sys.exit()
+
+
+
+
+
+
+f = sys.stdin
+s = f.read()
 f.close()
 
 words = s.split()
 
-d = {}for w in words:
-if d.has_key(w):
-d[w] += 1
-else:
-d[w] = 1sored_keys = sorted(d.keys(), key = lambda x: d[x], reverse = True)
+d = {}
 
-print "all: %d" % len(d)i = 0
-for k in sored_keys:    
-if i == 20:        
-break    
-print k, ": ", d[k]    
-i += 1
+for w in words:
+  if d.has_key(w):
+    d[w] += 1
+  else:
+    d[w] = 1
+      
+sored_keys = sorted(d.keys(), key = lambda x: d[x], reverse = True)
+print "all: %d" % len(d)
+
+i = 0
+for k in sored_keys:
+  if i == 20:
+  break
+  print k, ": ", d[k]
+  i += 1
